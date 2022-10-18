59384261003048b955b29c9feafb6fd06db55446
CLAUDIO-FLORES1 <CLAUDIOFLRS1@GMAIL.COM>
2022-10-17 22:18:34 -0400

    Readying app for deployment

--- models/connection.js
+++ models/connection.js
@@ -2,13 +2,14 @@ require("dotenv").config()
 const mongoose = require("mongoose")
 
 const DATABASE_URL = process.env.DATABASE_URL
+const DEPLOYED_URL = process.env.DEPLOYED_URL
 
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
 
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(DEPLOYED_URL, CONFIG)
 
 mongoose.connection
     .on("open", () => console.log("Connected to Mongoose"))
